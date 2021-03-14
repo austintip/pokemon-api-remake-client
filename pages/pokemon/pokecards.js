ef46fa04cc8eaaacbe167a9fa484fc961cd838bb
austintip <austin.d.tipograph@gmail.com>
2021-03-13 22:20:36 -0500

    Playing around with next and adding links

---
+++ pages/pokemon/pokecards.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function pokecards() {
+    return (
+        <>
+            <h1>Pokemon Cards!</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </>
+    )
+}
