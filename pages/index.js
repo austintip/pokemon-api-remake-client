49687618f4654cf8d4aa34ed811848205c91acb0
austintip <austin.d.tipograph@gmail.com>
2021-03-16 16:49:42 -0400

    data returning from fetch call

--- pages/index.js
+++ pages/index.js
@@ -5,48 +5,77 @@ import axios from 'axios'
 
 export default function Home() {
 
-  const apiCall = (req, res) => {
-    axios({
-      url: 'https://humane-oarfish-32.hasura.app/v1/graphql',
-      method: 'post',
-      headers: {
-        // Authorization: `x-hasura-admin-secret 123123123`
-        'x-hasura-admin-secret': '123123123'
-      },
-      data: {
-        query: `{
-          pokemon_api_pokemon {
-            pokedex_number
-            name
-            first_type
-            second_type
-            height
-            weight_lbs
-            first_ability
-            second_ability
-            hidden_ability
-          }}`
-      }
-    }).then(pokemon => {
-      console.log(pokemon.data)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+  // const apiCall = (req, res) => {
+  //   axios({
+  //     url: 'https://humane-oarfish-32.hasura.app/v1/graphql',
+  //     method: 'post',
+  //     headers: {
+  //       // Authorization: `x-hasura-admin-secret 123123123`
+  //       'x-hasura-admin-secret': '123123123'
+  //     },
+  //     data: {
+  //       query: `{
+  //         pokemon_api_pokemon {
+  //           pokedex_number
+  //           name
+  //           first_type
+  //           second_type
+  //           height
+  //           weight_lbs
+  //           first_ability
+  //           second_ability
+  //           hidden_ability
+  //         }}`
+  //     }
+  //   }).then(pokemon => {
+  //     console.log(pokemon.data)
+  //   }).catch(err => {
+  //     console.log(err)
+  //   })
+  // }
 
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Welcome to the new and *soon-to-be* improved version of the Pokemon API!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+  const query = `
+  query {
+    pokemon_api_pokemon {
+      pokedex_number
+      name
+      first_type
+      second_type
+      height
+      weight_lbs
+      first_ability
+      second_ability
+      hidden_ability
+    }
+  }`
+
+
+const url = 'https://humane-oarfish-32.hasura.app/v1/graphql'
+
+const opts = {
+  method: "POST",
+  headers: { 'x-hasura-admin-secret': '123123123' },
+  body: JSON.stringify({ query })
+}
+
+fetch(url, opts)
+  .then(res => res.json())
+  .then(console.log)
+  .catch(console.log)
+
+return (
+  <Layout home>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+      <p>Welcome to the new and *soon-to-be* improved version of the Pokemon API!</p>
+      <p>
+        (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
-      <button onClick={apiCall}>CALL THE API HERE</button>
-    </Layout>
-  )
+    </section>
+    <button>CALL THE API HERE</button>
+  </Layout>
+)
 }
